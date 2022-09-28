26189c67d66f2e20117045c2192317cf6fbed4db
brittany <brittanyfreitas9@gmail.com>
2022-09-28 09:29:30 -0400

    added slide out nav feature

--- app.js
+++ app.js
@@ -6,39 +6,19 @@
 //on click of the menu icon, add class of hidden on current nav ie. display none, simultaneously, addClass of slideoutNav2 to the slideout nav.
 
 
-
+//setting my varibles to be able to target the menu icon and the exit icon
 const menuButton = document.querySelector('.menuIcon')
 const closeNav = document.querySelector('.exit');
 const slideMenu = document.querySelector('.slideOutNav')
 
+//attaching an event listener to my menu so that on click it adds the class of .open
 menuButton.addEventListener ("click", function(){
-  
-    slideMenu.className = 'slideOutNav2';
+    slideMenu.classList.add('open');
 });
 
-
+//attaching an event listener to my exit icon such that when clicked it removes the class of .open
 closeNav.addEventListener ("click", function(){
-    slideMenu.className = 'slideOutNav';
+    slideMenu.classList.remove('open');
 });
 
-
-//creating functions to open and close the nav
-// const openSlideMenu = (event) => {
-//     const show = event.target
-//     show.className = 'slideOutNav2';
-// }
-
-// const closeSlideMenu = () => {
-// }
-
-
-//('click', function () {
-//     // slideMunu 
-//     console.log('you clicked the button')
-// });
-
-//setting the event lsiteners 
-// closeNav.addEventListener("click", closeSlideMenu, { once: true })
-
-// slideMenu.addEventListener("click", openSlideMenu)
 
